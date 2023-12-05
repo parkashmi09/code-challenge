8d9cffa2d1156aa595ebf6ba0821a114f8073634
parkash <parkashmi66@gmail.com>
2023-12-05 15:58:16 +0530

    fix: remove path

--- server/index.js
+++ server/index.js
@@ -11,18 +11,12 @@ dotenv.config();
 
 const PORT = 5001;
 
-const __dirname= path.resolve()
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use("/api", useRouter);
-
-app.use(express.static(path.join(__dirname,"/client/dist")));
-app.get("*", (req, res)=>{
-  res.sendFile(path.join(__dirname,"client", "dist", "index.html"))
-})
 
 // async function insertData(selectors) {
 //     try {
